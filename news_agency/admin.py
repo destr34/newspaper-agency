b7afb576213ab313b4998dc578feddc22f74952e
Vitalii Shulepa <vitaliy.shulep@gmail.com>
2023-05-17 19:06:07 +0300

    Add admin.py classes (TopicAdmin, NewspaperAdmin, RedactorAdmin)

--- news_agency/admin.py
+++ news_agency/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from news_agency.models import Topic, Newspaper, Redactor
+
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    search_fields = ["title"]
+
+
+@admin.register(Newspaper)
+class NewspaperAdmin(admin.ModelAdmin):
+    search_fields = ["title"]
+    list_filter = ["title", "publishers"]
+    list_display = ["title", "topic", "published_date"]
+
+
+@admin.register(Redactor)
+class RedactorAdmin(UserAdmin):
+    search_fields = ["first_name", "last_name"]
+    list_display = UserAdmin.list_display + ("years_of_experience",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("years_of_experience",)}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "years_of_experience",
+                    )
+                },
+            ),
+        )
+    )
