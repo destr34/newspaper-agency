e373d50c4fa342772c5edf332203ddda2f6a4565
Vitalii Shulepa <vitaliy.shulep@gmail.com>
2023-05-17 18:25:48 +0300

    Add clases (Topic, Redactor, Newspaper) to models.py, update settings.py and make migrations

--- news_agency/models.py
+++ news_agency/models.py
@@ -1,3 +1,46 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from newspaper import settings
+
+
+class Topic(models.Model):
+    topic = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ["topic"]
+
+    def __str__(self) -> str:
+        return self.topic
+
+
+class Redactor(AbstractUser):
+    years_of_experience = models.IntegerField()
+
+    class Meta:
+        ordering = ["first_name"]
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    published_date = models.DateTimeField()
+    topic = models.ForeignKey(
+        Topic, on_delete=models.CASCADE, related_name="newspaper"
+    )
+    publishers = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="newspaper"
+    )
+
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self) -> str:
+        return (
+            f"{self.publishers} published "
+            f"{self.title} date {self.published_date}"
+        )
--- newspaper/settings.py
+++ newspaper/settings.py
@@ -101,6 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "news_agency.Redactor"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
