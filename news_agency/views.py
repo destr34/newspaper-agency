ec009130a6130637249e5959344d2a85554811b5
Vitalii Shulepa <vitaliy.shulep@gmail.com>
2023-05-18 11:59:48 +0300

    Add urls.py to news_agency folder, add index function to views.py

--- news_agency/views.py
+++ news_agency/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from news_agency.models import Topic, Redactor, Newspaper
+
+
+def index(request):
+    count_topic = Topic.objects.count()
+    count_redactor = Redactor.objects.count()
+    count_newspaper = Newspaper.objects.count()
+
+    context = {
+        "count_topic": count_topic,
+        "count_redactor": count_redactor,
+        "count_newspaper": count_newspaper
+    }
+
+    return render(request, "news-agency/index.html", context=context)
