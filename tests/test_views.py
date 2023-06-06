f810fac3df6985952c37e0e037d0be93208da7bb
Vitalii Shulepa <vitaliy.shulep@gmail.com>
2023-06-07 00:30:54 +0300

    Add PublicRedactorTests and PrivateRedactorTests to test_views.py

--- tests/test_views.py
+++ tests/test_views.py
@@ -2,12 +2,15 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
-from news_agency.models import Topic, Newspaper
+from news_agency.models import Topic, Newspaper, Redactor
+
 
 TOPIC_URL = reverse("news_agency:topic-list")
+REDACTOR_URL = reverse("news_agency:redactor-list")
+NEWSPAPER_URL = reverse("news_agency:newspaper-list")
 
 class PublicTopicTests(TestCase):
-    def test_login_required(self):
+    def test_login_required(self) -> None:
         res = self.client.get(TOPIC_URL)
 
         self.assertNotEqual(res.status_code, 200)
@@ -21,7 +24,7 @@ class PrivateTopicTests(TestCase):
         )
         self.client.force_login(self.user)
 
-    def test_retrieve_topic(self):
+    def test_retrieve_topic(self) -> None:
         Topic.objects.create(topic="news_test")
         Topic.objects.create(topic="news_test2")
 
@@ -44,7 +47,7 @@ class PrivateNewspaperTest(TestCase):
         )
         self.client.force_login(self.user)
 
-    def test_retrieve_newspaper(self):
+    def test_retrieve_newspaper(self) -> None:
         topic = Topic.objects.create(topic="Test_topic")
         Newspaper.objects.create(
             topic=topic,
@@ -53,11 +56,40 @@ class PrivateNewspaperTest(TestCase):
         )
 
         newspaper = Newspaper.objects.all()
-        response = self.client.get(
-            reverse("news_agency:newspaper-list")
-        )
+        response = self.client.get(NEWSPAPER_URL)
+
         self.assertEqual(response.status_code, 200)
         self.assertEqual(
             list(response.context["newspaper_list"]),
             list(newspaper)
+        )
+
+
+class PublicRedactorTest(TestCase):
+        def test_login_required(self) -> None:
+            res = self.client.get(REDACTOR_URL)
+
+            self.assertNotEqual(res.status_code, 200)
+
+
+class PrivateRedactorTest(TestCase):
+    def setUp(self) -> None:
+        self.user = get_user_model().objects.create_user(
+            "Adam_test",
+            "password123"
+        )
+        self.client.force_login(self.user)
+
+    def test_retrieve_redactor(self) -> None:
+        Redactor.objects.create_user(
+            "Sam",
+            "test1234",
+        )
+        redactor = Redactor.objects.all()
+        response = self.client.get(REDACTOR_URL)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            list(response.context["redactor_list"]),
+            list(redactor)
         )
